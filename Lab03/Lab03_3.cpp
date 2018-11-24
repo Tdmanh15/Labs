6e6cd73590191bfca1f38fff6255f499619eb808
Tdmanh15 <44284693+Tdmanh15@users.noreply.github.com>
2018-11-24 10:11:00 +0700

    add new file Lab03_3

--- Lab03/Lab03_3.cpp
+++ Lab03/Lab03_3.cpp
@@ -1,1 +1,37 @@
-
+#include<iostream>
+#include<vector>
+using namespace std;
+class counter
+{
+	private:
+		static int count;
+		int id;
+		char name[20];
+	public :
+		counter()
+		{
+			static int count=0;
+			id=++count;
+		}
+		void enter()
+		{
+			cout<<"Enter customer name :";
+			cin>>name;
+		}
+		void display()
+		{
+			cout<<"Name : "<<name<<endl;
+			cout<<"ID : "<<id<<endl;
+		}
+};
+int main()
+{
+	counter x[2];
+	{
+		for(int i=0;i<2;i++)
+	{
+			x[i].enter();
+		x[i].display();
+	}
+}
+}
